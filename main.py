3f1ea3aba9dc5429338a8f3fd5015d75fc9377f1
roadwide <qq820925995@gmail.com>
2020-04-15 09:46:18 +0800

    standardized package

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from WEIBO import WEIBO
+#第一次详细了解package 从WEIBO这个包中导入WEIBO这个模块的WEIBO这个函数/类
+from WEIBO.WEIBO import WEIBO
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
+    # 只要是一个能登陆的微博账号就行
     w = WEIBO("username", "password")
+    # 要查询的微博ID
     w.WBID = "WBID"
     w.login()
     w.get_all_follow()
